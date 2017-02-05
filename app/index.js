b6233e2ffb82fb3f68ec61c7b6150698b90647f8
MarekLabuz <mrk.labuz@gmail.com>
2017-02-05 22:51:43 +0100

    redis setup

--- app/index.js
+++ app/index.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom'
 
 import Test from './components/Test'
 
 ReactDOM.render(
   <Test />,
-  document.getElementById('root')
+  document.getElementById('root')  // eslint-disable-line
 )
 
 if (module.hot) {
--- index.js
+++ index.js
@@ -3,29 +3,37 @@ const http = require('http')
 const history = require('connect-history-api-fallback')
 const config = require('./webpack.config')
 const webpack = require('webpack')
+const redis = require('redis')
+const bluebird = require('bluebird')
+
+bluebird.promisifyAll(redis.RedisClient.prototype)
+bluebird.promisifyAll(redis.Multi.prototype)
+const client = redis.createClient()
 
 const port = process.env.PORT || 5000
-
 const compiler = webpack(config)
-
 const app = express()
 const server = http.createServer(app)
 
 app.get('/hello', async (req, res) => {
-  res.send('hello world!')
+  const data = await client.getAsync('string key')
+  res.send(data)
+})
+
+client.set('string key', 'string val', () => {
+  client.getAsync('string key').then((res) => {
+    console.log(res)
+  })
 })
 
 app.use(history({
   index: '/index.html'
 }))
-
 app.use(express.static('static'))
-
 app.use(require('webpack-dev-middleware')(compiler, {
   noInfo: true,
   publicPath: config.output.publicPath
 }))
-
 app.use(require('webpack-hot-middleware')(compiler))
 
 server.listen(port, '0.0.0.0', (err) => {
