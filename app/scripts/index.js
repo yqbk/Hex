3b8858abdc2864d4793ca0adf61057d2482ab7bc
yqbk <kuba1994>
2017-02-06 23:35:44 +0100

    create map

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,7 +1,29 @@
+import _ from 'lodash'
 import * as PIXI from 'pixi.js'
+
 
 let renderer
 let stage
+
+function createMap (width, height, x) {
+
+  const unit = x * Math.sqrt(3) / 2
+
+  // const line = [
+  //   ..._.range(1, width + 1).map(id => new Hex(id * (2 * unit + x), 100, unit)),
+  //   ..._.range(1, width + 1).map(id => new Hex(id * (2 * unit + x) + unit + x / 2, 100 + unit, unit))
+  // ]
+
+  const line = _.range(0, height).reduce((acc, curr) => {
+    return [
+      ...acc,
+      ..._.range(1, width + 1).map(id => new Hex(id * (2 * unit + x) + (curr % 2 === 0 ? 0 : unit + x / 2), 100 + curr * unit, unit))
+    ]
+  }, [])
+
+  return line
+
+}
 
 class Hex {
   constructor (x, y, s = 50) {
@@ -11,16 +33,22 @@ class Hex {
     this.y = y
     this.s = s
 
+    this.selected = false
+
     this.hex.buttonMode = true
     this.hex.click = () => {
+
+      this.selected = !this.selected
+
       this.hex.clear()
-      this.render(0x00FFFF)
+      this.render(this.selected ? 0x00FF00 : 0xFF0000)
+
     }
   }
 
   render (color = 0x66CCFF) {
     const s = this.s
-    this.hex.lineStyle(2, 0xFF3300, 1)
+    // this.hex.lineStyle(2, 0xFF3300, 1)
     this.hex.beginFill(color)
     const halfWidth = (s * Math.sqrt(3)) / 2
     const relativePoints = [
@@ -60,8 +88,7 @@ export function init () {
 
   stage = new PIXI.Container()
 
-  const hex1 = new Hex(100, 100, 20)
-  hex1.render()
+  createMap(10,20,30).forEach(hex => hex.render())
 
 
   renderer.render(stage)
