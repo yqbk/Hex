5d695a3d61871d01f67c9f1b9370199300dd6d21
MarekLabuz <mrk.labuz@gmail.com>
2017-02-06 22:10:07 +0100

    hex with action

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,34 +1,60 @@
 import * as PIXI from 'pixi.js'
 
 let renderer
+let stage
 
 class Hex {
-  constructor () {
+  constructor (x, y, s = 50) {
+    this.hex = new PIXI.Graphics()
+    this.hex.interactive = true
+    this.x = x
+    this.y = y
+    this.s = s
 
+    this.hex.buttonMode = true
+    this.hex.click = () => {
+      console.log('sdfsdfsdf')
+    }
   }
 
   render () {
+    const s = this.s
+    this.hex.lineStyle(2, 0xFF3300, 1)
+    this.hex.beginFill(0x66CCFF)
+    const halfWidth = (s * Math.sqrt(3)) / 2
+    const relativePoints = [
+      -halfWidth, -s / 2,
+      0, -s,
+      halfWidth, -s / 2,
+      halfWidth, s / 2,
+      0, s,
+      -halfWidth, s / 2
+    ]
+    this.hex.drawPolygon(relativePoints)
+    this.hex.endFill()
+    this.hex.x = this.x
+    this.hex.y = this.y
+    this.hex.rotation = Math.PI / 2
 
+    this.hex.hitArea = new PIXI.Polygon(...relativePoints)
+
+    stage.addChild(this.hex)
   }
 }
 
 export function init () {
-  const map = document.getElementById('map')
-  renderer = PIXI.autoDetectRenderer(500, 500)
+  const map = document.getElementById('map') // eslint-disable-line
+  const WIDTH = window.innerWidth // eslint-disable-line
+  const HEIGHT = window.innerHeight // eslint-disable-line
+
+  renderer = PIXI.autoDetectRenderer(WIDTH, HEIGHT, null, true, true)
 
   map.appendChild(renderer.view)
 
-  const stage = new PIXI.Container()
+  stage = new PIXI.Container()
 
-
-  var rectangle = new PIXI.Graphics()
-  rectangle.lineStyle(4, 0xFF3300, 1)
-  rectangle.beginFill(0x66CCFF)
-  rectangle.drawRect(0, 0, 64, 64)
-  rectangle.endFill()
-  rectangle.x = 170
-  rectangle.y = 170
-  stage.addChild(rectangle)
+  const hex1 = new Hex(100, 100)
+  hex1.render()
 
 
   renderer.render(stage)
