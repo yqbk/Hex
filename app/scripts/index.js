3e364ddc4bf66a0052a75710615477d9c7323052
yqbk <jksyrek@gmail.com>
2017-02-07 18:14:58 +0100

    select fields on click

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -5,58 +5,6 @@ let stage
 let app
 let grid
 
-function createMap (width, height, x) {
-
-  const line = _.range(0, height).reduce((acc, curr) => [
-      ...acc,
-      ..._.range(1, width + 1).map(id => new Hex( id * x + (curr % 2 === 0 ? 0 : x / 2 ), 100 + curr * (x - 10)))
-    ], [])
-
-  return line
-}
-
-
-class Hex {
-  constructor (x, y, scale = 0.5) {
-    this.hex = new PIXI.Sprite(PIXI.Texture.fromImage('water.png'))
-    this.hex.interactive = true
-    this.hex.buttonMode = true
-
-    // center the water's anchor point
-    this.hex.anchor.set(0.5)
-    this.hex.scale.set(scale)
-    this.hex.x = x
-    this.hex.y = y
-
-    this.hex
-      .on('pointerdown', onDragStart)
-      .on('pointerup', onDragEnd)
-      .on('pointerupoutside', onDragEnd)
-      .on('pointermove', onDragMove)
-      .on('resize', onResize)
-
-    this.app = app
-    this.stage = stage
-
-    // this.selected = false
-    // this.hex.click = () => {
-    //   this.selected = !this.selected
-    //
-    //   this.hex.clear()
-    //   this.render(this.selected ? 0x00FF00 : 0x66CCFF)
-    // }
-  }
-
-  changePosition (moveX, moveY) {
-    this.hex.x += moveX
-    this.hex.y += moveY
-  }
-
-  render () {
-    app.stage.addChild(this.hex)
-  }
-}
-
 // ----- hex movement
 
 function onDragStart (event) {
@@ -66,6 +14,8 @@ function onDragStart (event) {
   this.data = event.data
   this.alpha = 0.5
   this.dragging = true
+
+  // this.hex.selected = true
 }
 
 function onResize (event) {
@@ -99,6 +49,58 @@ function onDragMove () {
   }
 }
 
+class Hex {
+  constructor (x, y, scale = 0.5) {
+    this.hex = new PIXI.Sprite(PIXI.Texture.fromImage('water.png'))
+    this.hex.interactive = true
+    this.hex.buttonMode = true
+
+    // center the water's anchor point
+    this.hex.anchor.set(0.5)
+    this.hex.scale.set(scale)
+    this.hex.x = x
+    this.hex.y = y
+
+    this.hex
+      .on('pointerdown', onDragStart)
+      .on('pointerup', onDragEnd)
+      .on('pointerupoutside', onDragEnd)
+      .on('pointermove', onDragMove)
+      .on('resize', onResize)
+
+    this.app = app
+    this.stage = stage
+
+    this.selected = false
+    this.hex.click = () => {
+      this.selected = !this.selected
+
+      console.log('item selected')
+      this.hex.tint = this.selected ? 0x00FF00 : 0xFFFFFF
+
+      // this.hex.clear()
+      // this.render(this.selected ? 0x00FF00 : 0x66CCFF)
+    }
+  }
+
+  changePosition (moveX, moveY) {
+    this.hex.x += moveX
+    this.hex.y += moveY
+  }
+
+  render () {
+    app.stage.addChild(this.hex)
+  }
+}
+
+function createMap (width, height, x) {
+  const line = _.range(0, height).reduce((acc, curr) => [
+    ...acc,
+    ..._.range(1, width + 1).map(id => new Hex(id * x + (curr % 2 === 0 ? 0 : x / 2), 100 + curr * (x - 10)))
+  ], [])
+
+  return line
+}
 
 export function init () {
   const map = document.getElementById('map') // eslint-disable-line
@@ -114,9 +116,6 @@ export function init () {
 
   // grid = _.range(1, 10 + 1).map(id => new Hex(100 * id, 300))
 
-  grid = createMap(10, 10, 85)
+  grid = createMap(15, 15, 85)
   grid.forEach(el => el.render())
-
-
-
 }
