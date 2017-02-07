9e8ad0fff730d02044ba0a1d694f179ae9412567
yqbk <jksyrek@gmail.com>
2017-02-07 17:50:02 +0100

    move hex grid

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,187 +1,151 @@
 import _ from 'lodash'
 import * as PIXI from 'pixi.js'
 
-let renderer
 let stage
+let app
+let grid
 
-function createMap (width, height, x, app) {
-  const unit = x * Math.sqrt(3) / 2
-
-  const line = _.range(0, height).reduce((acc, curr) => {
-    return [
-      ...acc,
-      ..._.range(1, width + 1).map(id => new Hex(id * (2 * unit + x) + (curr % 2 === 0 ? 0 : unit + x / 2), 2 * x + curr * ( unit - 3 ) , unit, app))
-    ]
-  }, [])
-
-  return line
-}
+// function createMap (width, height, x) {
+//   // const unit = x * Math.sqrt(3) / 2
+//
+//   const line = _.range(0, height).reduce((acc, curr) => [
+//       ...acc,
+//       ..._.range(1, width + 1).map(id => new Hex(id * (2 * unit + x) + (curr % 2 === 0 ? 0 : unit + x / 2), 2 * x + curr * (unit - 3), unit, app))
+//     ], [])
+//
+//   return line
+// }
 
 class Hex {
-  constructor (x, y, s = 50, app) {
-    this.hex = new PIXI.Graphics()
+  constructor (x, y, scale = 0.5) {
+    this.hex = new PIXI.Sprite(PIXI.Texture.fromImage('water.png'))
     this.hex.interactive = true
-    this.x = x
-    this.y = y
-    this.s = s
-    this.app = app
-
-    this.selected = false
-
     this.hex.buttonMode = true
-    this.hex.click = () => {
 
-      this.selected = !this.selected
+    // center the water's anchor point
+    this.hex.anchor.set(0.5)
+    this.hex.scale.set(scale)
+    this.hex.x = x
+    this.hex.y = y
 
-      this.hex.clear()
-      this.render(this.selected ? 0x00FF00 : 0x66CCFF)
+    this.hex
+      .on('pointerdown', onDragStart)
+      .on('pointerup', onDragEnd)
+      .on('pointerupoutside', onDragEnd)
+      .on('pointermove', onDragMove)
+      .on('resize', onResize)
 
-    }
+    this.app = app
+    this.stage = stage
+
+    // this.selected = false
+    // this.hex.click = () => {
+    //   this.selected = !this.selected
+    //
+    //   this.hex.clear()
+    //   this.render(this.selected ? 0x00FF00 : 0x66CCFF)
+    // }
   }
 
-  render (color = 0x66CCFF) {
-    const s = this.s
-    // this.hex.lineStyle(3, 0x000000, 1)
-    this.hex.beginFill(color)
-    const halfWidth = (s * Math.sqrt(3)) / 2
-    const relativePoints = [
-      -halfWidth, -s / 2,
-      0, -s,
-      halfWidth, -s / 2,
-      halfWidth, s / 2,
-      0, s,
-      -halfWidth, s / 2
-    ]
-    this.hex.drawPolygon(relativePoints)
-    this.hex.endFill()
-    this.hex.x = this.x
-    this.hex.y = this.y
-    this.hex.rotation = Math.PI / 2
-    // this.hex.antialias = true
+  changePosition (moveX, moveY) {
+    this.hex.x += moveX
+    this.hex.y += moveY
+  }
 
-    this.hex.hitArea = new PIXI.Polygon(...relativePoints)
-
-    this.app.stage.addChild(this.hex);
+  render () {
+    app.stage.addChild(this.hex)
   }
 }
 
+// ----- hex movement
 
-function gameLoop () {
-  requestAnimationFrame(gameLoop) // eslint-disable-line
-  renderer.render(stage)
+function onDragStart (event) {
+  // store a reference to the data
+  // the reason for this is because of multitouch
+  // we want to track the movement of this particular touch
+  this.data = event.data
+  this.alpha = 0.5
+  this.dragging = true
 }
+
+function onResize (event) {
+  // store a reference to the data
+  // the reason for this is because of multitouch
+  // we want to track the movement of this particular touch
+  this.scale = event.data
+}
+
+function onDragEnd () {
+  this.alpha = 1
+  this.dragging = false
+  // set the interaction data to null
+  this.data = null
+}
+
+function onDragMove () {
+  if (this.dragging) {
+    const newPosition = this.data.getLocalPosition(this.parent)
+
+    const moveX = -(this.x - newPosition.x)
+    const moveY = -(this.y - newPosition.y)
+
+
+    // this.x = newPosition.x
+    // this.y = newPosition.y
+
+    grid.forEach((el) => {
+      el.changePosition(moveX, moveY)
+    })
+  }
+}
+
 
 export function init () {
   const map = document.getElementById('map') // eslint-disable-line
   const WIDTH = window.innerWidth // eslint-disable-line
   const HEIGHT = window.innerHeight // eslint-disable-line
 
-  const app = new PIXI.Application(WIDTH, HEIGHT, { transparent: true });
-  map.appendChild(app.view);
+  app = new PIXI.Application(WIDTH, HEIGHT, { transparent: true })
+  map.appendChild(app.view)
 
-  const container = new PIXI.Container();
+  const container = new PIXI.Container()
 
-  app.stage.addChild(container);
+  app.stage.addChild(container)
 
-  //--------------------------
-  // renderer = PIXI.autoDetectRenderer(WIDTH, HEIGHT, null, true, true)
-  // renderer.backgroundColor = 0xFFFFFF
+  grid = _.range(1, 10 + 1).map(id => new Hex(100 * id, 300))
+  grid.forEach(el => el.render())
+
+
+  // ----- create hex
+
+  // function createWater (x, y) {
+  //   const water = new PIXI.Sprite(texture)
+  //   water.interactive = true
   //
-  // map.appendChild(renderer.view)
-
-  // stage = new PIXI.Container()
-
-  // createMap(15,20,40,app).forEach(hex => hex.render())
-
-  var texture = PIXI.Texture.fromImage('water.png');
-
-
-
-  function createBunny(x, y) {
-
-    // create our little bunny friend..
-    var bunny = new PIXI.Sprite(texture);
-
-    // enable the bunny to be interactive... this will allow it to respond to mouse and touch events
-    bunny.interactive = true;
-
-    // this button mode will mean the hand cursor appears when you roll over the bunny with your mouse
-    bunny.buttonMode = true;
-
-    // center the bunny's anchor point
-    bunny.anchor.set(0.5);
-
-    // make it a bit bigger, so it's easier to grab
-    bunny.scale.set(0.5);
-
-    // setup events for mouse + touch using
-    // the pointer events
-    bunny
-      .on('pointerdown', onDragStart)
-      .on('pointerup', onDragEnd)
-      .on('pointerupoutside', onDragEnd)
-      .on('pointermove', onDragMove)
-      .on('resiza', onResize);
-
-    // For mouse-only events
-    // .on('mousedown', onDragStart)
-    // .on('mouseup', onDragEnd)
-    // .on('mouseupoutside', onDragEnd)
-    // .on('mousemove', onDragMove);
-
-    // For touch-only events
-    // .on('touchstart', onDragStart)
-    // .on('touchend', onDragEnd)
-    // .on('touchendoutside', onDragEnd)
-    // .on('touchmove', onDragMove);
-
-    // move the sprite to its designated position
-    bunny.x = x;
-    bunny.y = y;
-
-    // add it to the stage
-    app.stage.addChild(bunny);
-  }
-
-  function onDragStart(event) {
-    // store a reference to the data
-    // the reason for this is because of multitouch
-    // we want to track the movement of this particular touch
-    this.data = event.data;
-    this.alpha = 0.5;
-    this.dragging = true;
-  }
-
-  function onResize(event) {
-    // store a reference to the data
-    // the reason for this is because of multitouch
-    // we want to track the movement of this particular touch
-    this.scale = event.data;
-  }
-
-  function onDragEnd() {
-    this.alpha = 1;
-    this.dragging = false;
-    // set the interaction data to null
-    this.data = null;
-  }
-
-  function onDragMove() {
-    if (this.dragging) {
-      var newPosition = this.data.getLocalPosition(this.parent);
-      this.x = newPosition.x;
-      this.y = newPosition.y;
-    }
-  }
-
-  createBunny(300,300)
-
-
-  // let test = new Hex(100,100,40, app)
+  //   // this button mode will mean the hand cursor appears when you roll over the water with your mouse
+  //   water.buttonMode = true
   //
-  // test.render()
+  //   // center the water's anchor point
+  //   water.anchor.set(0.5)
+  //
+  //   // make it a bit bigger, so it's easier to grab
+  //   water.scale.set(0.5)
+  //
+  //   water
+  //     .on('pointerdown', onDragStart)
+  //     .on('pointerup', onDragEnd)
+  //     .on('pointerupoutside', onDragEnd)
+  //     .on('pointermove', onDragMove)
+  //     .on('resize', onResize)
+  //
+  //   water.x = x
+  //   water.y = y
+  //
+  //   app.stage.addChild(water)
+  //
+  //   return water
+  // }
 
 
-  // renderer.render(stage)
-  // gameLoop()
+  // const waterTable = _.range(1, 10 + 1).map(id => new Hex(100 * id, 300, app))
 }
